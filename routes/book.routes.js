1bd11d9cc36d6b1c4ffbe3684ba1f11554869527
Louise <louisegabriellecc@gmail.com>
2022-04-25 14:57:47 +0100

    Create new book added

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -3,7 +3,7 @@ const Book = require("../models/Book.model");
 const router = require("express").Router();
 
 
-router.get('/book', (req, res, next) => {
+router.get('/books', (req, res, next) => {
     Book.find()
     .then((booksArray) => {
        console.log("these books were found", booksArray);
@@ -12,7 +12,29 @@ router.get('/book', (req, res, next) => {
     .catch(err => console.log("A error occuried searching for books on db" ,err))
 });
 
-router.get('/book/:bookId', (req, res, next) =>{
+router.get('/books/new', (req, res, next) => {
+    res.render("new-book")
+})
+
+router.post('/books/new', (req, res, next) =>{
+    console.log(req.body)
+    const newBook = {
+        title: req.body.title,
+        author: req.body.author,
+        description: req.body.description,
+        rate: req.body.rating,
+    }
+    Book.create(newBook)
+    .then(newBook => {
+        res.redirect("/books")
+        console.log("A new book was created", newBook)
+    })
+    .catch(err =>console.log("There was an error creating a new book", err))
+})
+
+
+
+router.get('/books/:bookId', (req, res, next) =>{
     const id = req.params.bookId
 
     Book.findById(id)
@@ -21,5 +43,8 @@ router.get('/book/:bookId', (req, res, next) =>{
         res.render("individual-book", bookDetails)
     })
 })
+
+
+
 
 module.exports = router;
