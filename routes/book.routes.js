3472c5357be04c0fbb233cf2cde02614cba5c593
Louise <louisegabriellecc@gmail.com>
2022-04-26 09:39:14 +0100

    filter function now working correctly

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -3,6 +3,7 @@ const Book = require("../models/Book.model");
 const router = require("express").Router();
 
 
+// Display all books - READ
 router.get('/books', (req, res, next) => {
     Book.find()
     .then((booksArray) => {
@@ -12,17 +13,20 @@ router.get('/books', (req, res, next) => {
     .catch(err => console.log("A error occuried searching for books on db" ,err))
 });
 
+
+// Render a form to CREATE new book
 router.get('/books/new', (req, res, next) => {
     res.render("new-book")
 })
 
-router.post('/books/new', (req, res, next) =>{
+// CREATE new book - process the form
+router.post('/books/create', (req, res, next) =>{
     console.log(req.body)
     const newBook = {
         title: req.body.title,
         author: req.body.author,
         description: req.body.description,
-        rate: req.body.rating,
+        rating: req.body.rating,
     }
     Book.create(newBook)
     .then(newBook => {
@@ -33,7 +37,7 @@ router.post('/books/new', (req, res, next) =>{
 })
 
 
-
+// Display individual book details - READ
 router.get('/books/:bookId', (req, res, next) =>{
     const id = req.params.bookId
 
@@ -44,6 +48,7 @@ router.get('/books/:bookId', (req, res, next) =>{
     })
 })
 
+// Edit individual book details - render form - UPDATE
 router.get('/books/:bookId/edit', (req, res, next) =>{
     const id = req.params.bookId
 
@@ -54,6 +59,7 @@ router.get('/books/:bookId/edit', (req, res, next) =>{
     .catch(err => console.log("There was an error fetching the book to be edited", err))
 });
 
+// Edit individual book details - proccess from - UPDATE
 router.post('/books/:bookId/edit', (req, res, next) =>{
 
     const id = req.params.bookId
@@ -62,7 +68,7 @@ router.post('/books/:bookId/edit', (req, res, next) =>{
         title: req.body.title,
         author: req.body.author,
         description: req.body.description,
-        rate: req.body.rating,
+        rating: req.body.rating,
     }
     Book.findByIdAndUpdate(id, updateBook)
     .then((updatedBook) => {
@@ -70,6 +76,33 @@ router.post('/books/:bookId/edit', (req, res, next) =>{
     })
     .catch(err =>console.log("There was an error updating a book", err))
 })
+
+
+// Delete a book  from
+router.post('/books/:bookId/delete', (req, res, next) => {
+    const id = req.params.bookId
+    Book.findByIdAndRemove(id)
+    .then(() =>
+    res.redirect("/books"))
+    .catch(err => console.log("There wasan error deleting a book", err)
+        )
+})
+
+
+// Filter by rating
+router.get('/rating/:rating', (req, res, next) => {
+    const rate = req.params.rating
+    const bookRating = parseInt(rate)
+    console.log("User searched by rating", bookRating)
+    Book.find({rating: {$gt :bookRating}})
+    .then((booksByRatingArray) => {
+       console.log("these books were found", booksByRatingArray);
+        res.render("books-filtered", {booksRating: booksByRatingArray});
+    })
+    .catch(err => console.log("A error occuried returning books by rating" ,err))
+});
+
+
 
 
 module.exports = router;
