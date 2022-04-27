2486a1bb82e11fc564937ac3cc5a729b930ad773
Louise <louisegabriellecc@gmail.com>
2022-04-27 12:00:42 +0100

    auth: functionality to register

---
+++ routes/auth.routes.js
@@ -0,0 +1,40 @@
+const User = require("../models/User.model");
+const router = require("express").Router();
+const bcryptjs = require("bcryptjs");
+const saltRounds = 10;
+
+//render register form
+router.get("/register", (req, res, next) => {
+    res.render("auth/register");
+})
+
+
+//process register form - generate SALT and HASH
+router.post("/register", (req, res, next) => {
+    
+    const {email, password} = req.body //ES6 object destructuring
+
+    bcryptjs
+    .genSalt(saltRounds)
+    .then( salt => {
+      return bcryptjs.hash(password, salt)
+    })
+    .then( hashedPassword => {
+        console.log("hashedPassowrd", hashedPassword);
+        const userDetails = {
+            email: email,
+            passwordHash: hashedPassword
+        }
+        return User.create(userDetails)
+    })
+    .then( user => {
+
+        res.send("user was created :)")
+    })
+   
+    .catch(err => {console.log("error creating account", err)
+    next(err);})
+})
+
+
+module.exports = router;
